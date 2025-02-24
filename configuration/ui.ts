64ed326b2f4af282b02ee546140fcb55ed9f5d6a
acm9312 <acm9312@gmail.com>
2025-02-24 10:42:56 -0500

    Update ui.ts
    Changed AI name and owner name

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `AI by Anthony`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const PAGE_TITLE: string = `Anthony's AI Project`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with Valorem, Anthony's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";

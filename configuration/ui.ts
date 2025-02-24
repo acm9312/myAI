d66095a9362b5e53bf4feb48ac48b5e539649531
acm9312 <acm9312@gmail.com>
2025-02-24 10:25:48 -0500

    Update ui.ts
    Updated page title

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Time to Chat`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `AI by Anthony`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `Chat with Tony`;
+export const PAGE_TITLE: string = `Anthony's AI Project`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";

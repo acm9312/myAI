c831b76b6d3df617801c1b468a0d348a24b56292
acm9312 <acm9312@gmail.com>
2025-02-24 10:01:04 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,8 +1,8 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `LETS GOOOO`;
+export const CHAT_HEADER: string = `Time to Chat`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `AI by Miller`;
+export const FOOTER_MESSAGE: string = `AI by Anthony`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const PAGE_TITLE: string = `Chat with Tony`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;

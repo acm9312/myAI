8a819928940b76ab6f886e3f63dc1362c94078b7
acm9312 <acm9312@gmail.com>
2025-02-23 20:36:57 -0500

    Update ui.ts
    Few changes?

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Talk Talk Talk`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const FOOTER_MESSAGE: string = `Brought to you by MILLERTIME`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR CHAT`;
+export const PAGE_TITLE: string = `AI With Anthony`;
+export const PAGE_DESCRIPTION: string = `Chat with ${Valorem}, ${Anthony}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";

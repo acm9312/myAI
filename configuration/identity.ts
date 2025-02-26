7a10d5393515068dfce8710416b4dbb8fabd3637
acm9312 <acm9312@gmail.com>
2025-02-26 10:55:06 -0500

    Update identity.ts
    New AI name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Valorem`;
+export const OWNER_DESCRIPTION: string = `Anthony`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;

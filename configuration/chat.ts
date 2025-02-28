46b35772c748ab4f17f8347620d68dac6aabde24
acm9312 <acm9312@gmail.com>
2025-02-28 08:44:46 -0800

    Update chat.ts
    Updated some message prompts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
-export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
+export const INITIAL_MESSAGE: string = `What;s up!, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant or little helper monkey as he calls me.`;
+export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I am being weird right now. Please try again at a time that is not now.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
